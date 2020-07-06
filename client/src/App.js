f8ac4c18d4eeb503f067cb14d0b359a161315d8e
Sean Tan <sean.tan@mail.mcgill.ca>
2020-07-06 17:26:27 -0400

    Setup Client

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './App.css';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div className='App'>
+        <a href='http://localhost:8888'> Login to Spotify </a>
+      </div>
+    )
+  }
+}
