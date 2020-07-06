e7501e8288ae274b34bd5759e2d4fb58f985e5b4
Sean Tan <sean.tan@mail.mcgill.ca>
2020-07-06 17:59:37 -0400

    Added utils

---
+++ client/src/utils/index.js
@@ -0,0 +1,12 @@
+
+
+export const getHashParams = () => {
+    var hashParams = {};
+    var e, r = /([^&;=]+)=?([^&;]*)/g,
+      q = window.location.hash.substring(1);
+    while (e) {
+      hashParams[e[1]] = decodeURIComponent(e[2]);
+      e = r.exec(q);
+    }
+    return hashParams;
+  };
