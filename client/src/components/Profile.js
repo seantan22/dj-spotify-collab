a40de80f2f45122276bf5ea3f25a463383f9a2c7
Sean Tan <sean.tan@mail.mcgill.ca>
2020-07-06 21:54:18 -0400

    Update Profile.js

--- client/src/components/Profile.js
+++ client/src/components/Profile.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
 
+import User from '../components/User';
+
 export default class Profile extends Component {
   render() {
     return (
       <div>
         PROFILE
+        <User />
       </div>
     )
   }
