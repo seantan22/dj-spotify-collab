03b6aa263dae898a5bdb5a2841e1d6895b49524e
Sean Tan <sean.tan@mail.mcgill.ca>
2020-07-06 21:53:58 -0400

    Fixed getHashParams()

--- client/src/utils/index.js
+++ client/src/utils/index.js
@@ -2,12 +2,12 @@
 // Pull token from query string
 // Returns an object with the parameters as properties
 export const getHashParams = () => {
-    var hashParams = {};
-    var e, r = /([^&;=]+)=?([^&;]*)/g,
-      q = window.location.hash.substring(1);
-    while (e) {
-      hashParams[e[1]] = decodeURIComponent(e[2]);
-      e = r.exec(q);
-    }
-    return hashParams;
-  };
+  const hashParams = {};
+  let e;
+  const r = /([^&;=]+)=?([^&;]*)/g;
+  const q = window.location.hash.substring(1);
+  while ((e = r.exec(q))) {
+    hashParams[e[1]] = decodeURIComponent(e[2]);
+  }
+  return hashParams;
+};
