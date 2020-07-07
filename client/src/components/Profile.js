e3da424b1e6429972fd8e805296c77bc194d407f
Sean Tan <sean.tan@mail.mcgill.ca>
2020-07-07 12:09:36 -0400

    Added description

--- client/src/components/Profile.js
+++ client/src/components/Profile.js
@@ -6,7 +6,10 @@ export default class Profile extends Component {
   render() {
     return (
       <div>
-        PROFILE
+        Tempotify
+        <br/>
+        <br/>
+        When mixing music, a talented DJ will creatively fuse together song after song by executing technical transitions. These transitions take into account a variety of track characteristics, the most important being tempo. A general rule when choosing the next track is to stay within ±5 beats per minute of the previous track. This allows a DJ to smoothly blend two songs together without drawing too much attention from the audience.  Tempotify helps DJs identify songs that are similar in tempo so that they can build electric tracklists to keep the crowd on their feet.
         <User />
       </div>
     )
