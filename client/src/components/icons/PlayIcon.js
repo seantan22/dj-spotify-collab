658ab175c5b0c990a149c9e4d55d57f260fbf42f
Sean Tan <sean.tan@mail.mcgill.ca>
2020-07-09 18:57:29 -0400

    Create PlayIcon.js

---
+++ client/src/components/icons/PlayIcon.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const PlayIcon = () => (
+    <svg id="play-icon" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M3 22v-20l18 10-18 10z"/></svg>
+);
+
+export default PlayIcon;
