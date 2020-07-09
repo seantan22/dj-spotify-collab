ce25c189b11edaa2be9876122b7b67d3a721e1c8
Sean Tan <sean.tan@mail.mcgill.ca>
2020-07-09 18:57:49 -0400

    Create index.js

---
+++ client/src/components/icons/index.js
@@ -0,0 +1,15 @@
+import SpotifyIcon from './SpotifyIcon';
+import PlayIcon from './PlayIcon';
+import UserIcon from './UserIcon';
+import GitHubIcon from './GitHubIcon';
+import StarIcon from './StarIcon';
+import HalfStarIcon from './HalfStarIcon';
+
+export {
+  SpotifyIcon,
+  PlayIcon,
+  UserIcon,
+  GitHubIcon,
+  StarIcon,
+  HalfStarIcon,
+};
