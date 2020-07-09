7d9e4a0c212041418983206edd3cc79b2f5700c1
Sean Tan <sean.tan@mail.mcgill.ca>
2020-07-09 18:56:15 -0400

    Added categorization utils, date/time formatting

--- client/src/utils/index.js
+++ client/src/utils/index.js
@@ -1,4 +1,3 @@
-
 // Pull token from query string
 // Returns an object with the parameters as properties
 export const getHashParams = () => {
@@ -11,3 +10,109 @@ export const getHashParams = () => {
   }
   return hashParams;
 };
+
+// Error Handling
+export const catchErrors = fn => {
+  return function(...args) {
+    return fn(...args).catch(err => {
+      console.error(err);
+    });
+  };
+};
+
+// Get YYYY from YYYY-MM-DD
+export const getYear = date => date.split('-')[0];
+
+// Get 00:00m from 000000ms
+export const formatDuration = millis => {
+  const minutes = Math.floor(millis / 60000);
+  const seconds = ((millis % 60000) / 1000).toFixed(0);
+  return `${minutes}:${seconds < 10 ? '0' : ''}${seconds}`;
+};
+
+// Map Integers to Pitches Using Pitch Class Notation
+export const intToKey = int => {
+  let  key = int;
+  switch (int) {
+    case 0:
+      key = 'C';
+      break;
+    case 1:
+      key = 'D♭';
+      break;
+    case 2:
+      key = 'D';
+      break;
+    case 3:
+      key = 'E♭';
+      break;
+    case 4:
+      key = 'E';
+      break;
+    case 5:
+      key = 'F';
+      break;
+    case 6:
+      key = 'G♭';
+      break;
+    case 7:
+      key = 'G';
+      break;
+    case 8:
+      key = 'A♭';
+      break;
+    case 9:
+      key = 'A';
+      break;
+    case 10:
+      key = 'B♭';
+      break;
+    case 11:
+      key = 'B';
+      break;
+    default:
+      return null;
+  }
+
+  return key;
+};
+
+export const intToMode = int => {
+  if(int === 1) {
+    return 'Major';
+  } else {
+    return 'Minor';
+  }
+}
+
+export const categorizeEnergy = energy => {
+  if(energy >= .95) {
+    return 'Banger Alert';
+  } else  if (energy < .95 && energy >= .80) {
+    return 'High';
+  } else  if (energy < .80 && energy > .50) {
+    return 'Mid';
+  } else {
+    return 'Low';
+  }
+}
+
+export const categorizeDanceability = danceability => {
+  if(danceability >= .95) {
+    return 'Woah';
+  } else  if (danceability < .95 && danceability >= .80) {
+    return 'High';
+  } else  if (danceability < .80 && danceability > .50) {
+    return 'Mid';
+  } else {
+    return 'Low';
+  }
+}
+
+export const categorizeValence = valence => {
+  if(valence >= .50) {
+    return 'Positive';
+  } else {
+    return 'Negative';
+  }
+}
