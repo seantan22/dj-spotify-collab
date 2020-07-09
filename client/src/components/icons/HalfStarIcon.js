9d9e92aad4c02f2eb9f88c09ddedbc98e05f6a3d
Sean Tan <sean.tan@mail.mcgill.ca>
2020-07-09 18:57:31 -0400

    Create HalfStarIcon.js

---
+++ client/src/components/icons/HalfStarIcon.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const HalfStarIcon = () => (
+    <svg 
+        version="1.1" 
+        xmlns="http://www.w3.org/2000/svg"  
+        width="32"
+        height="32"
+        color="#b3b3b3"
+        viewBox="0 0 16 16">
+            <path d="M5.6 5.4l-5.5 0.8 3.9 3.8-0.9 5.5 4.9-2.6v-12.5z" />
+    </svg>
+);
+
+export default HalfStarIcon;
