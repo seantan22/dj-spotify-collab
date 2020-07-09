79a0a05974d56b95fe72fd1e6093a610667f87de
Sean Tan <sean.tan@mail.mcgill.ca>
2020-07-09 18:57:06 -0400

    Update Overview.js

--- client/src/components/Overview.js
+++ client/src/components/Overview.js
@@ -1,15 +1,29 @@
 import React, { Component } from 'react';
-import User from './User';
+import { Router } from '@reach/router';
+import styled from 'styled-components/macro';
+
+import NavBar from './NavBar';
 import NowPlaying from './NowPlaying';
+import Track from './Track';
+import Profile from './Profile';
+
+import theme from '../styles/theme';
+
+const SiteWrapper = styled.div`
+  padding-left: ${theme.navWidth};
+`;
 
 export default class Overview extends Component {
   render() {
     return (
-      <div>
-        <NowPlaying />
-        <User />
-        <br/>
-      </div>
+      <SiteWrapper>
+        <NavBar />
+        <Router primary={false}>
+          <NowPlaying path="/" />
+          <Profile path="profile" />
+          <Track path="track/:trackId" />
+        </Router>
+      </SiteWrapper>
     )
   }
 }
