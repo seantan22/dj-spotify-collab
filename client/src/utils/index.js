2bac1667186c3c6210a13c9039aaf38a4c860669
Sean Tan <sean.tan@mail.mcgill.ca>
2020-07-13 12:02:46 -0400

    Update index.js

--- client/src/utils/index.js
+++ client/src/utils/index.js
@@ -35,6 +35,51 @@ export const formatDuration = millis => {
   return `${minutes}:${seconds < 10 ? '0' : ''}${seconds}`;
 };
 
+export const dateMonthYear = date => {
+  let month = date.split('-')[1];
+  switch (date.split('-')[1]) {
+    case '01':
+      month = 'January';
+      break;
+    case '02':
+      month = 'February';
+      break;
+    case '03':
+      month = 'March';
+      break;
+    case '04':
+      month = 'April';
+      break;
+    case '05':
+      month = 'May';
+      break;
+    case '06':
+      month = 'June';
+      break;
+    case '07':
+      month = 'July';
+      break;
+    case '08':
+      month = 'August';
+      break;
+    case '09':
+      month = 'September';
+      break;
+    case '10':
+      month = 'October';
+      break;
+    case '11':
+      month = 'November';
+      break;
+    case '12':
+      month = 'December';
+      break;
+    default:
+      return null;
+  }
+  return month + ' ' + date.split('-')[0]
+}
+
 // Map Integers to Pitches Using Pitch Class Notation
 export const intToKey = int => {
   let  key = int;
