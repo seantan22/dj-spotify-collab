819d5eea60806ce49dff3b3f0d31136f753d4eec
Sean Tan <sean.tan@mail.mcgill.ca>
2020-07-20 21:48:09 -0400

    NavBar responsive

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -6,9 +6,8 @@ import { SpotifyIcon, BeatSwitchIcon, PlayIcon, UserIcon, GitHubIcon, SearchIcon
 import styled from 'styled-components/macro';
 import theme from '../styles/theme';
 import mixins from '../styles/mixins';
+import media from '../styles/media';
 const { colors } = theme;
-
-
 
 const Container = styled.nav`
   ${mixins.coverShadow};
@@ -22,8 +21,20 @@ const Container = styled.nav`
   background-color: ${colors.navBlack};
   text-align: center;
   z-index: 99;
+  ${media.tablet`
+    top: auto;
+    bottom: 0;
+    right: 0;
+    width: 100%;
+    min-height: ${theme.navHeight};
+    height: ${theme.navHeight};
+    flex-direction: row;
+  `};
   & > * {
     width: 100%;
+    ${media.tablet`
+      height: 100%;
+    `};
   }
 `;
 
@@ -32,14 +43,12 @@ const Logo = styled.div`
   margin-top: 30px;
   width: 70px;
   height: 70px;
-  // transition: ${theme.transition};
-  // &:hover,
-  // &:focus {
-  //   color: ${colors.brightGreen};
-  // }
   svg {
     width: 50px;
   }
+  ${media.tablet`
+    display: none;
+  `}
 `;
 
 const GitHub = styled.div`
@@ -54,6 +63,9 @@ const GitHub = styled.div`
       width: 30px;
     }
   }
+  ${media.tablet`
+    display: none;
+  `};
 `;
 
 const Spotify = styled.div`
@@ -69,21 +81,41 @@ const Spotify = styled.div`
       width: 30px;
     }
   }
+  ${media.tablet`
+    display: none;
+  `}; 
 `;
 
 const Menu = styled.ul`
   display: flex;
   flex-direction: column;
+  ${media.tablet`
+    flex-direction: row;
+    align-items: flex-end;
+    justify-content: center;
+  `};
 `;
 const MenuItem = styled.li`
   color: ${colors.lightGrey};
   font-size: 11px;
+  ${media.tablet`
+    flex-grow: 1;
+    flex-basis: 100%;
+    height: 100%;
+  `};
   a {
     display: block;
     padding: 15px 0;
     border-left: 5px solid transparent;
     width: 100%;
     height: 100%;
+    ${media.tablet`
+      ${mixins.flexCenter};
+      flex-direction: column;
+      padding: 0;
+      border-left: 0;
+      border-top: 3px solid transparent;
+    `};
     }
     &:hover, &:focus {
         color: ${colors.white};
@@ -95,6 +127,10 @@ const MenuItem = styled.li`
         background-color: ${colors.black};
         border-left: 5px solid ${colors.brightRed};
         pointer-events: none;
+        ${media.tablet`
+          border-left: 0;
+          border-top: 3px solid ${colors.offGreen};
+        `};
     }
   }
   svg {
